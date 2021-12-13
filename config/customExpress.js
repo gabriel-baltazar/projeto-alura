0ba2bb40877a0857f93ed3902554c3027a87276d
Gabriel Baltazar <gabrielaltazar@hotmail.com>
2021-12-13 08:16:59 -0300

    Config mysql2 and add table atendimentos

--- config/customExpress.js
+++ config/customExpress.js
@@ -3,6 +3,7 @@ const consign = require('consign');
 
 module.exports = () => {
     const app = express();
+    app.use(express.json());
 
     consign()
     .include('controllers')
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,4 +2,9 @@ module.exports = app => {
   app.get('/atendimentos', (req, res) => {
     res.send('Pagina de atendimentos');
   });
+
+  app.post('/atendimentos', (req, res) => {
+    console.log(req.body);
+    res.send('Cadastro de atendimento');
+  });
 }
