a3884d31780b7289ec2ea2ed63ae8222cf0b2f71
Gabriel Baltazar <gabrielaltazar@hotmail.com>
2022-02-12 21:23:47 -0300

    Api finalizada

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,31 @@
-module.exports = app => {
-  app.get('/atendimentos', (req, res) => {
-    res.send('Pagina de atendimentos');
-  });
+const Atendimento = require('../models/atendimentos')
 
-  app.post('/atendimentos', (req, res) => {
-    console.log(req.body);
-    res.send('Cadastro de atendimento');
-  });
+module.exports = app => {
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        Atendimento.buscaPorId(id,res)
+    })
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = (req.body)
+        Atendimento.altera(id,valores,res)
+    })
+
+    app.delete('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        Atendimento.deleta(id,res)
+    })
+
+    app.post('/atendimentos', (req, res) => {
+        const atendimento = req.body
+        Atendimento.adiciona(atendimento, res)
+
+    })
+     
+     
 }
--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
-
 const conexao = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'agenda-petshop' 
-});
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'agenda-petshop'
+})
 
 module.exports = conexao;
